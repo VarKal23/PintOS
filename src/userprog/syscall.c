85bde53b78058025d55a45adb16520c4a35fde7c
VarKal23 <varunk@utexas.edu>
2023-03-03 15:04:37 -0600

    fixed up syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,6 +9,19 @@
 #include "filesys/file.h"
 
 static void syscall_handler (struct intr_frame *);
+static void halt();
+static void exit(void* esp);
+static void exec(void* esp, struct intr_frame* f);
+static void wait(void* esp, struct intr_frame* f);
+static void create(void* esp, struct intr_frame* f, char* file_name);
+static void remove(void* esp, struct intr_frame* f);
+static void open(struct intr_frame* f, char* file_name);
+static void write(void* esp, struct intr_frame* f, char* buf);
+static void read(void* esp, struct intr_frame* f, char* buf);
+static void filesize(void* esp, struct intr_frame* f);
+static void seek(void* esp);
+static void tell(void* esp, struct intr_frame* f);
+static void close(void* esp);
 struct lock file_lock;
 
 void acquire_file_lock () {
@@ -41,9 +54,6 @@ static void syscall_handler (struct intr_frame *f UNUSED)
 {
   void *esp = f->esp;
   if (!valid_pointer (esp)) {
-    // TODO: call process exit?
-    // and with what status?
-    // remember you changed the call in some other files too
     thread_exit (-1);
     return;
   }
@@ -51,205 +61,208 @@ static void syscall_handler (struct intr_frame *f UNUSED)
   int syscall_number = *(int *) esp;
   esp = (char*) esp + 4;
 
+  // Validates arguments of various arg count
+  if (syscall_number == SYS_CREATE || SYS_SEEK) {
+    if (!valid_args(esp, 2)) {
+      thread_exit(-1);
+      return;
+    }
+  } else if (syscall_number == SYS_WRITE || SYS_READ) {
+    if (!valid_args(esp, 3)) {
+      thread_exit(-1);
+      return;
+    }
+  } else {
+    if (!valid_args(esp, 1)) {
+      thread_exit(-1);
+      return;
+    }
+  }
+
   if (syscall_number == SYS_HALT) {
-    shutdown_power_off ();
+    halt();
 
   } else if (syscall_number == SYS_EXIT) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
-    int status = *(int *) esp;
-    // printf("%d", status);
-    thread_exit (status);
+    exit(esp);
 
   } else if (syscall_number == SYS_EXEC) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
-    char* cmd_line = *(char **) esp;
-    f->eax = process_execute (cmd_line);
+    exec(esp, f);
 
   } else if (syscall_number == SYS_WAIT) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
-    tid_t tid = *(tid_t *) esp;
-    f->eax = process_wait (tid);
+    wait(esp, f);
 
   } else if (syscall_number == SYS_CREATE) {
-     if (!valid_args (esp, 2)) {
-      thread_exit (-1);
-      return;
-    }
-    
     char* file_name = *(char**) esp;
     if (!valid_pointer (file_name)) {
       thread_exit (-1);
       return;
     }
-    unsigned int initial_size = *(unsigned int*) ((char*) esp + 4);
-    lock_acquire (&file_lock);
-    f->eax = filesys_create (file_name, initial_size);
-    lock_release (&file_lock);
+    create(esp, f, file_name);
 
   } else if (syscall_number == SYS_REMOVE) {
-     if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
-
-    char* file_name = *(char**) esp;
-    lock_acquire (&file_lock);
-    f->eax = filesys_remove (file_name);
-    lock_release (&file_lock);
+    remove(esp, f);
 
   } else if (syscall_number == SYS_OPEN) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
     char* file_name = *(char**) esp;
     if (!valid_pointer (file_name)) {
       thread_exit (-1);
       return;
     }
-    lock_acquire (&file_lock);
-    struct file* fp = filesys_open (file_name);
-    lock_release (&file_lock);
-
-    struct thread* cur_thread = thread_current();
-    if (!fp) {
-      f->eax = -1;
-    } else {
-      int index = 2;
-      while (cur_thread->fdt[index] != NULL) {
-        index++;
-      }
-      cur_thread->fdt[index] = fp;
-      f->eax = index;
-    } 
+    open(f, file_name);
 
   } else if (syscall_number == SYS_WRITE) {
-    if (!valid_args (esp, 3)) {
-      thread_exit (-1);
-      return;
-    }
-
-    int fd = *(int *) esp;
-    // TODO: increment esp pointer?
     char* buf = *(char**) ((char*) esp + 4);
     if (!valid_pointer (buf)) {
       thread_exit (-1);
       return;
     }
-    unsigned size = *(unsigned*) ((char*) esp + 8);
-    // TODO: validate buffer?
+    write(esp, f, buf);
 
-    if (fd == 1) {
-      lock_acquire (&file_lock);
-      putbuf (buf, size);
-      lock_release (&file_lock);
-      f->eax = size;
-    } else {
-      struct file* file = thread_current ()->fdt[fd];
-      if (!file) {
-        f->eax = -1;
-      } 
-      lock_acquire (&file_lock);
-      f->eax = file_write (file, buf, size);
-      lock_release (&file_lock);
-    }
   } else if (syscall_number == SYS_READ) {
-    // TODO: change up this function
-    if (!valid_args (esp, 3)) {
-      thread_exit (-1);
-      return;
-    }
-    int fd = *(int *) esp;
     char* buf = *(char**) ((char*) esp + 4);
     if (!valid_pointer (buf)) {
       thread_exit (-1);
       return;
     }
-    unsigned size = *(unsigned*) ((char*) esp + 8);
-    if (fd == 0) {
-      lock_acquire (&file_lock);
-      for (int i = 0; i < size; i++) {
-        buf[i] = input_getc ();
-      }
-      lock_release (&file_lock);
-      f->eax = size;
-    } else {
-      struct file* file = thread_current ()->fdt[fd];
-      // printf("%p", file);
-      if (!file) {
-        f->eax = -1;
-      } 
-      lock_acquire (&file_lock);
-      f->eax = file_read (file, buf, size);
-      lock_release (&file_lock);
-    }
-  } else if (syscall_number == SYS_FILESIZE) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
+    read(esp, f, buf);
 
-    int fd = *(int *) esp;
-    struct file* file = thread_current ()->fdt[fd];
-    if (!file) {
-      f->eax = -1;
-    }
-    // TODO: move locks?
-    lock_acquire (&file_lock);
-    f->eax = file_length (file);
-    lock_release (&file_lock);
+  } else if (syscall_number == SYS_FILESIZE) {
+    filesize(esp, f);
+
   } else if (syscall_number == SYS_SEEK) {
-    if (!valid_args (esp, 2)) {
-      thread_exit (-1);
-      return;
-    }
-    int fd = *(int *) esp;
-    unsigned int pos = *(unsigned int*) ((char*) esp + 4);
-    struct file* file = thread_current ()->fdt[fd];
-    if (file) {
-      lock_acquire (&file_lock);
-      file_seek(file, pos);
-      lock_release (&file_lock);
-    }
+    seek(esp);
 
   } else if (syscall_number == SYS_TELL) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
+    tell(esp, f);
 
-    int fd = *(int *) esp;
+  } else if (syscall_number == SYS_CLOSE) {
+    close(esp);
+  }
+}
+
+static void halt() {
+  shutdown_power_off ();
+}
+
+static void exit(void* esp) {
+  int status = *(int *) esp;
+  thread_exit (status);
+}
+static void exec(void* esp, struct intr_frame* f) {
+  char* cmd_line = *(char **) esp;
+  f->eax = process_execute (cmd_line);
+}
+static void wait(void* esp, struct intr_frame* f) {
+  tid_t tid = *(tid_t *) esp;
+  f->eax = process_wait (tid);
+}
+static void create(void* esp, struct intr_frame* f, char* file_name) {
+  unsigned int initial_size = *(unsigned int*) ((char*) esp + 4);
+  lock_acquire (&file_lock);
+  f->eax = filesys_create (file_name, initial_size);
+  lock_release (&file_lock);
+}
+static void remove(void* esp, struct intr_frame* f) {
+  char* file_name = *(char**) esp;
+  lock_acquire (&file_lock);
+  f->eax = filesys_remove (file_name);
+  lock_release (&file_lock);
+}
+static void open(struct intr_frame* f, char* file_name) {
+  lock_acquire (&file_lock);
+  struct file* fp = filesys_open (file_name);
+  lock_release (&file_lock);
+
+  struct thread* cur_thread = thread_current();
+  if (!fp) {
+    f->eax = -1;
+  } else {
+    int index = 2;
+    while (cur_thread->fdt[index] != NULL) {
+      index++;
+    }
+    cur_thread->fdt[index] = fp;
+    f->eax = index;
+  } 
+}
+static void write(void* esp, struct intr_frame* f, char* buf) {
+  int fd = *(int *) esp;
+  unsigned size = *(unsigned*) ((char*) esp + 8);
+  if (fd == 1) {
+    lock_acquire (&file_lock);
+    putbuf (buf, size);
+    lock_release (&file_lock);
+    f->eax = size;
+  } else {
     struct file* file = thread_current ()->fdt[fd];
     if (!file) {
       f->eax = -1;
-    } else {
-      lock_acquire (&file_lock);
-      f->eax = file_tell(file);
-      lock_release (&file_lock);
+    } 
+    lock_acquire (&file_lock);
+    f->eax = file_write (file, buf, size);
+    lock_release (&file_lock);
+  }
+}
+static void read(void* esp, struct intr_frame* f, char* buf) {
+  int fd = *(int *) esp;
+  unsigned size = *(unsigned*) ((char*) esp + 8);
+  if (fd == 0) {
+    lock_acquire (&file_lock);
+    for (int i = 0; i < size; i++) {
+      buf[i] = input_getc ();
     }
-
-  } else if (syscall_number == SYS_CLOSE) {
-    if (!valid_args (esp, 1)) {
-      thread_exit (-1);
-      return;
-    }
-    int fd = *(int *) esp;
+    lock_release (&file_lock);
+    f->eax = size;
+  } else {
     struct file* file = thread_current ()->fdt[fd];
-    if (file) {
-      lock_acquire (&file_lock);
-      file_close(file);
-      thread_current ()->fdt[fd] = NULL;
-      lock_release (&file_lock);
-      // free(file);
-    }
+    if (!file) {
+      f->eax = -1;
+    } 
+    lock_acquire (&file_lock);
+    f->eax = file_read (file, buf, size);
+    lock_release (&file_lock);
+  }
+}
+static void filesize(void* esp, struct intr_frame* f) {
+  int fd = *(int *) esp;
+  struct file* file = thread_current ()->fdt[fd];
+  if (!file) {
+    f->eax = -1;
+  }
+  lock_acquire (&file_lock);
+  f->eax = file_length (file);
+  lock_release (&file_lock);
+}
+
+static void seek(void* esp) {
+  int fd = *(int *) esp;
+  unsigned int pos = *(unsigned int*) ((char*) esp + 4);
+  struct file* file = thread_current ()->fdt[fd];
+  if (file) {
+    lock_acquire (&file_lock);
+    file_seek(file, pos);
+    lock_release (&file_lock);
+  }
+}
+static void tell(void* esp, struct intr_frame* f) {
+  int fd = *(int *) esp;
+  struct file* file = thread_current ()->fdt[fd];
+  if (!file) {
+    f->eax = -1;
+  } else {
+    lock_acquire (&file_lock);
+    f->eax = file_tell(file);
+    lock_release (&file_lock);
+  }
+}
+static void close(void* esp) {
+  int fd = *(int *) esp;
+  struct file* file = thread_current ()->fdt[fd];
+  if (file) {
+    lock_acquire (&file_lock);
+    file_close(file);
+    thread_current ()->fdt[fd] = NULL;
+    lock_release (&file_lock);
+    // free(file);
   }
 }
